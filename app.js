a0b80589c1418426fd601f2e0e4125b3d80db052
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-12-24 21:16:18 +0530

    registration

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const authUtils = require('./utils/auth');
 const hbs = require('hbs');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const authRouter = require('./routes/auth');
 var app = express();
 
 MongoClient.connect('mongodb://localhost', (err, client) => {
@@ -81,6 +82,7 @@ app.use((req, res, next) => {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
